863f97a62b8a6090c705ad3966d74b84d3cf7641
George-IRR <georgeradu169@gmail.com>
2026-03-20 12:09:19 +0200

    fix: == to |-> and other assertion ideas

--- debug/tb/interface_APB.sv
+++ debug/tb/interface_APB.sv
@@ -46,12 +46,14 @@ interface interface_APB #(
     else $error("[PROTOCOL VIOLATION] paddr_i unstable in ACCESS PHASE at: %0t", $time);
 
   property pready_psel_is_eq;
-  @(posedge clk_i) pready_o == psel_i;
+  @(posedge clk_i) pready_o |-> psel_i;
   endproperty
 
   assert property (pready_psel_is_eq)
     else $error("[PROTOCOL VIOLATION] pready is not equal to psel at: %0t", $time);
   
-
-
+  //penable |-> psel
+  //$fell(pready) |-> $(penable)
+  //$fell(psel) |-> $fell(pready)
+  //$fell(psel) |-> $fell(penalbe)
 endinterface
