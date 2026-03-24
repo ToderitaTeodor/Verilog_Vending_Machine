cb29ffca5ca0d09844b790907c44475226a3b2f9
Ungureanu Marius-Daniel <ungureanumarius67@gmail.com>
2026-03-24 15:05:30 +0200

    test(APB): add transaction, generator, driver, monitor

--- debug/tb/input_apb_transaction.sv
+++ debug/tb/input_apb_transaction.sv
@@ -3,41 +3,41 @@
 //-------------------------------------------------------------------------
 
 //aici se declara tipul de data folosit pentru a stoca datele vehiculate intre generator si driver; monitorul, de asemenea, preia datele de pe interfata, le recompune folosind un obiect al acestui tip de data, si numai apoi le proceseaza
-class transaction;
+class input_apb_transaction;
   //se declara atributele clasei
   //campurile declarate cu cuvantul cheie rand vor primi valori aleatoare la aplicarea functiei randomize()
-  rand bit [1:0] addr;
-  rand bit       wr_en;
-  rand bit       rd_en;
-  rand bit [7:0] wdata;
-       bit [7:0] rdata;
-       bit [1:0] cnt;
+
+  rand bit [7:0] addr;
+  rand bit   	 pwrite;
+  rand bit [7:0] data;
+  rand int delay_between_transaction;
   
   //constrangerile reprezinta un tip de membru al claselor din SystemVerilog, pe langa atribute si metode
   //aceasta constrangere specifica faptul ca se executa fie o scriere, fie o citire
   //constrangerile sunt aplicate de catre compilator atunci cand atributele clasei primesc valori aleatoare in urma folosirii functiei randomize
-  constraint wr_rd_c { wr_en != rd_en; }; 
+  constraint delay_c { delay_between_transaction inside {[1:20]};} 
   
-     constraint wdata_c;
+    
   
   //aceasta functie este apelata dupa aplicarea functiei randomize() asupra obiectelor apartinand acestei clase
   //aceasta functie afiseaza valorile aleatorizate ale atributelor clasei
   function void post_randomize();
     $display("--------- [Trans] post_randomize ------");
     //$display("\t addr  = %0h",addr);
-    if(wr_en) $display("\t addr  = %0h\t wr_en = %0h\t wdata = %0h",addr,wr_en,wdata);
-    if(rd_en) $display("\t addr  = %0h\t rd_en = %0h",addr,rd_en);
+    if(pwrite) $display("Tranzactie de scriere: \t addr  = %0h\t wr_en = %0h\t wdata = %0h \t delay = %0d",addr,pwrite,data, delay_between_transaction);
+    if(!pwrite) $display("Tranactie de citire: \t addr  = %0h\t wr_en = %0h\t wdata = %0h \t delay = %0d",addr,pwrite,data, delay_between_transaction);
     $display("-----------------------------------------");
   endfunction
   
   //operator de copiere a unui obiect intr-un alt obiect (deep copy)
-  function transaction do_copy();
-    transaction trans;
+  function input_apb_transaction do_copy();
+    input_apb_transaction trans;
     trans = new();
-    trans.addr  = this.addr;
-    trans.wr_en = this.wr_en;
-    trans.rd_en = this.rd_en;
-    trans.wdata = this.wdata;
+    trans.addr   = this.addr;
+    trans.pwrite = this.pwrite;
+    trans.data   = this.data;
+    trans.delay_between_transaction = this.delay_between_transaction;
+    
     return trans;
   endfunction
 endclass
